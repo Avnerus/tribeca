c58d15431740e8c56e31b06ed32493b7aba57cb9
Avner Peled <avnerus@gmail.com>
2015-05-28 16:13:20 +0300

    update

---
+++ src/app/objects/eye.js
@@ -0,0 +1,8 @@
+'use strict'
+import GameObject from './game-object';
+
+export default class Eye extends GameObject {
+    constructor() {
+        super();
+    }     
+};
--- src/app/stores.js
+++ src/app/stores.js
@@ -1,10 +1,8 @@
-import FruitStore from './stores/fruit';
 import AuthStore from './stores/auth';
 import MainStore from './stores/main';
 
 export default class Stores {
     constructor() {
-        this.fruit = new FruitStore();
         this.auth = new AuthStore();
         this.main = new MainStore();
     }
