47947453d54b8dff36483049d2cc95e250b02a67
Ido Adler <redturt@gmail.com>
2015-05-29 11:57:26 +0300

    my eyes

--- src/app/objects/eye.js
+++ src/app/objects/eye.js
@@ -6,22 +6,83 @@ export default class Eye extends GameObject {
         super();
 
         console.log("Initialising Eye");
-        this.texEyes = PIXI.Texture.fromFrame("assets/eyes.png");
-        this.texBlink = PIXI.Texture.fromFrame("assets/blink.png");
+        this.texHappy = PIXI.Texture.fromFrame("assets/happy.png");
+        this.texMad = PIXI.Texture.fromFrame("assets/Mad.png");
+        this.texNoPupils = PIXI.Texture.fromFrame("assets/noPupils.png");
+        this.texNormal = PIXI.Texture.fromFrame("assets/normal.png");
+        this.texSad = PIXI.Texture.fromFrame("assets/sad.png");
+        this.texSurprised = PIXI.Texture.fromFrame("assets/surprised.png");
+        this.texPupil = PIXI.Texture.fromFrame("assets/Pupil.png");
+        this.texBlink = PIXI.Texture.fromFrame("assets/WINK.png");
 
-        this.sprite = new PIXI.Sprite(this.texEyes);
+        this.sprite = new PIXI.Sprite(this.texHappy);
         this.sprite.anchor.x = 0.5;
         this.sprite.anchor.y = 0.5;
-        this.sprite.scale.x = 0.2;
-        this.sprite.scale.y = 0.2;
+        this.sprite.scale.x = 1;
+        this.sprite.scale.y = 1;
     }
 
     blink()
     {
+        let lastTex = this.sprite.texture
         this.sprite.texture = this.texBlink;
         TweenMax.delayedCall(0.5, () => {
-            this.sprite.texture = this.texEyes;
+            this.sprite.texture = lastTex;
         });
     }
 
+    normal()
+    {
+        this.sprite.texture = this.texNormal;
+    }
+
+    happy()
+    {
+        this.sprite.texture = this.texHappy;
+    }
+
+    mad()
+    {
+        this.sprite.texture = this.texMad;
+    }
+
+    noPupils()
+    {
+        this.sprite.texture = this.texNoPupils;
+    }
+
+    sad()
+    {
+        this.sprite.texture = this.texSad;
+    }
+
+    surprised()
+    {
+        this.sprite.texture = this.texSurprised;
+    }
+
+    randomMood()
+    {
+        let mood = Math.floor(Math.random() * 5);
+        switch(mood) {
+            case 0:
+                this.happy();
+                break;
+            case 1:
+                this.mad();
+                break;
+            case 2:
+                this.noPupils();
+                break;
+            case 3:
+                this.sad();
+                break;
+            case 4:
+                this.surprised();
+                break;
+            default:
+                this.normal();
+                break;
+        }
+    }
 };
