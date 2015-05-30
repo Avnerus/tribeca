de540777d0056a10fd9aba19075ca58366cc3960
Avner Peled <avnerus@gmail.com>
2015-05-30 18:52:10 +0300

    Email working (with a right Wifi)

--- src/server/services/mail.js
+++ src/server/services/mail.js
@@ -1,8 +1,36 @@
-console.log("MAIL IMPORT");
+import nodemailer from 'nodemailer';
 export default function(req, res) {
     console.log("Maik service!", req.body, req.params);
     res.writeHead(200, {'content-type': 'application/json'});
     res.end("{'status': 'OK'}");
+
+    console.log("Sending mail");
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'BFF2point0@gmail.com',
+            pass: 'hatsilu20'
+        }
+    });
+    let mailOptions = {
+        from: 'BFF2.0 <BFF2point0@gmail.com>', // sender address
+        to: req.body.mail, // list of receivers
+        subject: 'BFF2.0 - Our Selfie!', // Subject line
+        text: 'Keep in touch?', // plaintext body
+        html: '<b>Keep in touch?</b>', // html body
+        attachments: [
+        {   // filename and content type is derived from path
+            path: 'selfies/' + req.body.photoid + '.jpg'
+        }]
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            return console.log("Error sending mail " + error);
+        }
+        console.log('Message sent: ' + info.response);
+    });
 
     return;   
 }
